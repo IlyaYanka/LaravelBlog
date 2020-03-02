45dda29850ea8603c9e6d4681e5450c65912ae42
Ilya <yankouskii@rambler.ru>
2020-03-02 18:34:02 +0300

    add edit and index views

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function() {
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
+Route::group(['namespace' => 'Blog\Admin', 'prefix' => 'admin/blog'], function() {
+    $methods = ['index', 'edit', 'store', 'update', 'create'];
+    Route::resource('categories', 'CategoryController')->names('blog.posts')
+      ->only($methods)
+      ->names('blog.admin.categories');
+});
 //Route::resource('rest', 'RestTestController')->names('restTest');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
