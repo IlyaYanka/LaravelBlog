e15623be4b813135de704c6226ba13de6675ee05
Ilya <yankouskii@rambler.ru>
2020-03-02 11:46:04 +0300

    done Auth, some controllers

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-29 08:55:31.
+    * Generated on 2020-03-01 16:10:53.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.17.1 on 2020-02-29 08:55:30.
+ * Generated for Laravel 6.17.1 on 2020-03-01 16:10:53.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
